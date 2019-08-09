b61934435f50b5959b7f977ed64826718308f479
Ben Doe <ben@cuttlefish.com>
2019-08-09 11:42:35 +0100

    normal convention is to have period with function call
    e.g. `.then` and `.catch`

--- src/webauthnregister.js
+++ src/webauthnregister.js
@@ -37,42 +37,42 @@ function webauthnRegister(key, callback){
 	key.publicKey.user.id = new Uint8Array(key.publicKey.user.id);
 
 	// console.log(key);
-	navigator.credentials.create({publicKey: key.publicKey}).
-	then(function (aNewCredentialInfo) {
-		// console.log("Credentials.Create response: ", aNewCredentialInfo);
-		var cd = JSON.parse(String.fromCharCode.apply(null, new Uint8Array(aNewCredentialInfo.response.clientDataJSON)));
-		if (key.b64challenge != cd.challenge) {
-			callback(false, 'key returned something unexpected (1)');
-		}
-		if ('https://'+key.publicKey.rp.name != cd.origin) {
-			return callback(false, 'key returned something unexpected (2)');
-		}
-		if (! ('type' in cd)) {  return callback(false, 'key returned something unexpected (3)'); }
-		if (cd.type != 'webauthn.create') { return callback(false, 'key returned something unexpected (4)'); }
-
-		var ao = [];
-		(new Uint8Array(aNewCredentialInfo.response.attestationObject)).forEach(function(v){ ao.push(v); });
-		var rawId = [];
-		(new Uint8Array(aNewCredentialInfo.rawId)).forEach(function(v){ rawId.push(v); });
-		var info = {
-			rawId: rawId,
-			id: aNewCredentialInfo.id,
-			type: aNewCredentialInfo.type,
-			response: {
-				attestationObject: ao,
-				clientDataJSON:
-				  JSON.parse(String.fromCharCode.apply(null, new Uint8Array(aNewCredentialInfo.response.clientDataJSON)))
+	navigator.credentials.create({publicKey: key.publicKey})
+		.then(function (aNewCredentialInfo) {
+			// console.log("Credentials.Create response: ", aNewCredentialInfo);
+			var cd = JSON.parse(String.fromCharCode.apply(null, new Uint8Array(aNewCredentialInfo.response.clientDataJSON)));
+			if (key.b64challenge != cd.challenge) {
+				callback(false, 'key returned something unexpected (1)');
 			}
-		};
-		callback(true, JSON.stringify(info));
-	}).
-	catch(function(aErr) {
-		if (("name" in aErr) && (aErr.name == "AbortError" || aErr.name == "NS_ERROR_ABORT") ||
-			aErr.name == 'NotAllowedError')
-		{
-			callback(false, 'abort');
-		} else {
-			callback(false, aErr.toString());
-		}
-	});
+			if ('https://'+key.publicKey.rp.name != cd.origin) {
+				return callback(false, 'key returned something unexpected (2)');
+			}
+			if (! ('type' in cd)) {  return callback(false, 'key returned something unexpected (3)'); }
+			if (cd.type != 'webauthn.create') { return callback(false, 'key returned something unexpected (4)'); }
+
+			var ao = [];
+			(new Uint8Array(aNewCredentialInfo.response.attestationObject)).forEach(function(v){ ao.push(v); });
+			var rawId = [];
+			(new Uint8Array(aNewCredentialInfo.rawId)).forEach(function(v){ rawId.push(v); });
+			var info = {
+				rawId: rawId,
+				id: aNewCredentialInfo.id,
+				type: aNewCredentialInfo.type,
+				response: {
+					attestationObject: ao,
+					clientDataJSON:
+					  JSON.parse(String.fromCharCode.apply(null, new Uint8Array(aNewCredentialInfo.response.clientDataJSON)))
+				}
+			};
+			callback(true, JSON.stringify(info));
+		})
+		.catch(function(aErr) {
+			if (("name" in aErr) && (aErr.name == "AbortError" || aErr.name == "NS_ERROR_ABORT") ||
+				aErr.name == 'NotAllowedError')
+			{
+				callback(false, 'abort');
+			} else {
+				callback(false, aErr.toString());
+			}
+		});
 }
