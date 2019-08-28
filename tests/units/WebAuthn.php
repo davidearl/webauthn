ba80ca3db83bc33b28087c68366b9248a5408269
Ben Doe <benjamindoeuk@gmail.com>
2019-08-28 11:24:36 +0100

    added example test case

--- tests/units/WebAuthn.php
+++ tests/units/WebAuthn.php
@@ -6,4 +6,12 @@ use atoum;
 
 class WebAuthn extends atoum
 {
+    public function testPublicKeyOptions()
+    {
+        $this
+            ->given($this->newTestedInstance('app.com'))
+            ->then
+                ->string($this->testedInstance->prepareChallengeForRegistration('test', 123))
+                ->isNotEmpty();
+    }
 }
